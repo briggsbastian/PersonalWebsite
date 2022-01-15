764d12d08c54358727c20342a381de68cd127d2b
Your Name <briggsbastianp@gmail.com>
2022-01-14 18:04:23 -0800

    v.1.0.3

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import Head from 'next/head'
 import {Box, Container} from '@chakra-ui/react'
 import Navbar from "../navbar.js"
@@ -20,3 +21,4 @@ const Main = ({children, router}) => {
 }
 
 export default Main
+/* eslint-enable */
